073beb08b17d7200f36815ea25a5a3c4b11c97e9
tianyu1 <tianyu1@huya.com>
2019-07-15 19:45:53 +0800

    [tutorial][anim]简单放大动画

--- lib/modules/tutorial/animationdemo.dart
+++ lib/modules/tutorial/animationdemo.dart
@@ -3,8 +3,62 @@ import 'package:flutter/material.dart';
 class TutorialAnimationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tutorial page"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          LogoApp(),
+        ],
+      ),
+    );
+    Column(
+      children: <Widget>[
+        LogoApp(),
+      ],
+    );
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LogoAppState();
+  }
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    animationController.forward();
   }
 
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
 }
